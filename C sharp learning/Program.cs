91397510d69afa568cec79e3a266780c15185936
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-03 17:01:20 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic; //массив, списки.
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+
+namespace C_sharp_learning
+{
+    
+    struct Book
+    {
+        public string title;
+        public string name;
+        public string author;
+    };
+
+    class MainClass
+    {   
+        public static void Main(string[] args)
+        {
+            Book book;
+            book.name = "Pushkin";
+            book.title = "Lolec";
+            book.author = "aaa";
+            Console.ReadLine();
+        }
+
+    }
+}
